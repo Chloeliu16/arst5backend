e44beadb0c231a218765adc6b30744805ea4d723
Xiaojun Liu <chloeliu2310@gmail.com>
2024-02-22 22:14:50 -0500

    'version1'

---
+++ src/main/java/com/example/arst5backend/Arst5backendApplication.java
@@ -0,0 +1,13 @@
+package com.example.arst5backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arst5backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arst5backendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/controller/SearchFlight.java
@@ -0,0 +1,20 @@
+package controller;
+
+import dto.SearchRequest;
+import dto.SearchResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class SearchFlight {
+
+    @GetMapping
+    public SearchResponse search(@RequestBody SearchRequest searchRequest) {
+        // Need to do the real search in database
+        SearchResponse searchResponse = new SearchResponse();
+        return searchResponse;
+    }
+}
